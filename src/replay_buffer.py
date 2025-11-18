7709540cc2b2f93d0babce6d3894dc3b64587938
vedaa <vedaas.anand@gmail.com>
2025-11-18 14:33:05 +0530

    feat: add methods to access data

--- src/replay_buffer.py
+++ src/replay_buffer.py
@@ -1,6 +1,7 @@
 import numpy as np
 from collections import deque
 import random
+import os
 
 class ReplayBuffer:    
     def __init__(self, max_size=100000):
@@ -23,3 +24,30 @@ class ReplayBuffer:
     
     def size(self):
         return len(self.buffer)
+    
+    def save_to_file(self, path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        states = np.array([e[0] for e in self.buffer])
+        actions = np.array([e[1] for e in self.buffer])
+        rewards = np.array([e[2] for e in self.buffer])
+        next_states = np.array([e[3] for e in self.buffer])
+        dones = np.array([e[4] for e in self.buffer])
+        np.savez_compressed(path, states=states, actions=actions, rewards=rewards, next_states=next_states, dones=dones)
+        print(f"Replay buffer saved to {path}")
+
+    def load_from_file(self, path, max_items=None):
+        data = np.load(path)
+        states = data['states']
+        actions = data['actions']
+        rewards = data['rewards']
+        next_states = data['next_states']
+        dones = data['dones']
+
+        count = len(states)
+        if max_items is not None:
+            count = min(count, max_items)
+
+        for i in range(count):
+            self.add(states[i], int(actions[i]), float(rewards[i]), next_states[i], bool(dones[i]))
+
+        print(f"Loaded {count} transitions from {path} into replay buffer")
